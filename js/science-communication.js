1b1c095cee7dcdeb1d119c1dd30d66b0bba4d722
paipai <paipai@jlu>
2023-04-26 18:10:19 +0800

    xpl提交于2023年4月26日18:09:53

---
+++ js/science-communication.js
@@ -0,0 +1,36 @@
+$(function () {
+    // --初始让第一个显示--
+    var text = $(".text-segment")
+    text.hide()
+    text.eq(0).show()
+    // for (var i = 0; i < 3; i++) {
+    //     $("#buttons-item1-selector"+ (i + 1)).click(function() {
+    //         // --其他全隐藏--
+    //         console.log(i);
+    //         text.hide()
+    //         // --显示自己的--
+    //         text.eq(i).show()
+    //     })
+    // }
+    $("#buttons-item1-selector"+ 1).click(function() {
+        // --其他全隐藏--
+        text.hide()
+        // --显示自己的--
+        text.eq(0).show()
+    })
+    $("#buttons-item1-selector"+ 2).click(function() {
+        // --其他全隐藏--
+        text.hide()
+        // --显示自己的--
+        text.eq(1).show()
+    })
+    $("#buttons-item1-selector"+ 3).click(function() {
+        // --其他全隐藏--
+        text.hide()
+        // --显示自己的--
+        text.eq(2).show()
+    })
+}) 
+$('.block').smoove({
+    offset: '40%'
+});
