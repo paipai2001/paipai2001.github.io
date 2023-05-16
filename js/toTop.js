09923976a1f90d9ce4f36bc9ea37c2d06e0ce691
qxuanooo <qxuanchen03@gmail.com>
2023-05-16 22:44:11 +0800

    update

--- js/toTop.js
+++ js/toTop.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {	
 			if($("meta[name=toTop]").attr("content")=="true"){
-				$("<div id='toTop'><img src='1.png'></div>").appendTo('body');
+				$("<div id='toTop'><img src='images/1.png'></div>").appendTo('body');
 				$("#toTop").css({
 					width: '50px',
 					height: '50px',
